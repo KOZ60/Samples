e6c7891fbf32316920a22403c5f8c5c902e7c9ae
komura_ko <komura.kohzoh.cb@nisseicom.co.jp>
2021-04-07 10:19:12 +0900

    MonthPicker

---
+++ MonthPickerSample/MonthPickerSample/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MonthPickerSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthPicker1 = new MonthPickerSample.MonthPicker();
+            this.SuspendLayout();
+            // 
+            // monthPicker1
+            // 
+            this.monthPicker1.Location = new System.Drawing.Point(69, 78);
+            this.monthPicker1.Name = "monthPicker1";
+            this.monthPicker1.Size = new System.Drawing.Size(178, 19);
+            this.monthPicker1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 174);
+            this.Controls.Add(this.monthPicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonthPicker monthPicker1;
+    }
+}
+
---
+++ MonthPickerSample/MonthPickerSample/MonthPicker.cs
@@ -0,0 +1,99 @@
+﻿namespace MonthPickerSample
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
+
+    public class MonthPicker : DateTimePicker
+    {
+        internal const string Default_CustomFormat = "yyyy/MM";
+
+        public MonthPicker()
+        {
+            base.Format = DateTimePickerFormat.Custom;
+            base.CustomFormat = Default_CustomFormat;
+        }
+
+        [DefaultValue(DateTimePickerFormat.Custom)]
+        public new DateTimePickerFormat Format {
+            get => base.Format;
+            set => base.Format = value;
+        }
+
+        [DefaultValue(Default_CustomFormat)]
+        public new string CustomFormat {
+            get => base.CustomFormat;
+            set => base.CustomFormat = value;
+        }
+
+        protected override void OnDropDown(EventArgs eventargs)
+        {
+            // ドロップダウンが表示されるとき月選択に切り替える
+            var calhwnd = SendMessage(Handle, DTM_GETMONTHCAL, IntPtr.Zero, IntPtr.Zero);
+            SendMessage(calhwnd, MCM_SETCURRENTVIEW, IntPtr.Zero, (IntPtr)MCMV_YEAR);
+            base.OnDropDown(eventargs);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NOFITY:
+                    var nmhdr = (NMHDR)m.GetLParam(typeof(NMHDR));
+                    switch (nmhdr.code)
+                    {
+                        case MCN_VIEWCHANGE:
+                            // 日選択に戻るとき閉じる
+                            var vc = (NMVIEWCHANGE)m.GetLParam(typeof(NMVIEWCHANGE));
+                            if (vc.dwNewView == MCMV_MONTH)
+                            {
+                                SendMessage(Handle, DTM_CLOSEMONTHCAL, IntPtr.Zero, IntPtr.Zero);
+                            }
+
+                            break;
+                    }
+                    base.WndProc(ref m);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+
+        private const int WM_NOFITY = 0x004e;
+
+        private const int DTM_FIRST = 0x1000;
+        private const int DTM_GETMONTHCAL = DTM_FIRST + 8;
+        private const int DTM_CLOSEMONTHCAL = DTM_FIRST + 13;
+
+        private const int MCM_SETCURRENTVIEW = 0x1000 + 32;
+
+        private const int MCN_VIEWCHANGE = -750;
+
+        private const uint MCMV_MONTH = 0;
+        private const uint MCMV_YEAR = 1;
+        private const uint MCMV_DECADE = 2;
+        private const uint MCMV_CENTURY = 3;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct NMHDR
+        {
+            public IntPtr hwndFrom;
+            public IntPtr idFrom;
+            public int code;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct NMVIEWCHANGE
+        {
+            public NMHDR nmhdr;
+            public uint dwOldView;
+            public uint dwNewView;
+        }
+    }
+}
