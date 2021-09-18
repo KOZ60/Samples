da3475860b94116bacfe97401f02a96f598a857e
KOZ6.0 <komura.kohzoh.cb@nisseicom.co.jp>
2021-09-18 12:23:18 +0900

    外部プロセスの LitView を読む

---
+++ GetListViewItem/GetListViewItem/CoTaskMem.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+
+namespace GetListViewItem
+{
+    public class CoTaskMem : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        public CoTaskMem(int cbSize) : base(true)
+        {
+            SetHandle(Marshal.AllocCoTaskMem(cbSize));
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            Marshal.FreeCoTaskMem(handle);
+            return true;
+        }
+
+        public IntPtr Address {
+            get {
+                return handle;
+            }
+        }
+    }
+}
