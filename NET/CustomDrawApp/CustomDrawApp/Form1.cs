182807b31241823dd33ad43e88dcd7501a08dbf9
KOZ6.0 <komura.kohzoh.cb@nisseicom.co.jp>
2022-02-02 07:53:41 +0900

    Move Folders

---
+++ NET/CustomDrawApp/CustomDrawApp/Form1.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomDrawApp
+{
+    using System.Windows.Forms;
+    using System.Drawing;
+    using System;
+    using System.Linq;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
